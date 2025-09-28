8b3699a25d0ce4a43c5f442ff86e3e97802aa0a0
Mahesh8188 <ddbambhava8182@gmail.com>
2025-09-29 00:50:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -115,7 +115,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = True
-URL = environ.get("FQDN", "progressive-emelina-vegamovienewbot-ad86079b.koyeb.app")
+URL = environ.get("FQDN", "https://progressive-emelina-vegamovienewbot-ad86079b.koyeb.app")
 
 #---------------------------------------------------------------
 #---------------------------------------------------------------
